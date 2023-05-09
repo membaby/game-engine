3bf7a7fead61caa8c230b5755e86916745767eea
abderrahman504 <rahmankhsherb@gmail.com>
2023-05-09 23:24:04 +0300

    In progress checkers controller

--- src/main/scala/Games/Checkers.scala
+++ src/main/scala/Games/Checkers.scala
@@ -4,15 +4,6 @@ import javax.swing._
 import java.awt.{Color, Component, GridLayout}
 import javax.swing.{JFrame, JLabel, JPanel}
 
-// Define a class that extends the GameState trait
-//class CheckersGameState extends GameState {
-//  // Override the abstract fields as desired
-//  override var rows = 8
-//  override var cols = 8
-//  override var turn = 0
-//  override var board = Array.ofDim[Char](rows, cols)
-//  override var history: List[Array[Array[Char]]] = List(board)
-//}
 
 object Checkers {
 
@@ -33,12 +24,78 @@ object Checkers {
   }
 
   val CheckersController = (input: String, state: Array[Any]) => {
-    new Array[Any](0)
+    var actualState = state
+    if (actualState == null){
+      actualState = get_init_state()
+    }
+    val board = state(3).asInstanceOf[Array[Array[Char]]]
+    val inputPattern = "[0-7][a-h] [0-7][a-h]".r
+    if (inputPattern.matches(input)){
+      val (destRow, destCol) = (input.charAt(3) - '0', input.charAt(4) - 'a')
+      val (srcRow, srcCol) = (input.charAt(0) - '0', input.charAt(1) - 'a')
+      val player1 = state(2).asInstanceOf[Int] % 2 == 0
+      if (board(destRow)(destCol) != ' '){
+        //Invalid Destination
+      }
+      else{
+        if (player1) {
+          if (board(srcRow)(srcCol) != '1') {
+            //Invalid Source
+          }
+          else {
+            var possibleDests = Array[(Int, Int)](4)
+            if (board(srcRow-1)(srcCol-1) == ' ') possibleDests(0) = (srcRow-1, srcCol-1)
+            else if (board(srcRow-2)(srcCol-2) == ' ') possibleDests(1) = (srcRow-2, srcCol-2)
+            if (board(srcRow - 1)(srcCol + 1) == ' ') possibleDests(2) = (srcRow - 1, srcCol + 1)
+            else if (board(srcRow - 2)(srcCol + 2) == ' ') possibleDests(3) = (srcRow - 2, srcCol + 2)
+            //Missing rest of implementation
+          }
+        }
+        else {
+          if (board(srcRow)(srcCol) != '2') {
+            //Invalid Source
+          }
+          else {
+            //Missing rest of implementation
+          }
+      }
+
+      }
+
+    }
+    else{
+      //Invalid Input
+    }
   }
 
   val CheckersDrawer = (CurrentState: Array[Any]) => {
 
   }
 
+  private def get_init_state(): Array[Any] ={
+    var state = Array[Any](5)
+    val (row, col, turn) = (8, 8, 0)
+    val board = Array.fill(8)(Array.fill(8)(' '))
+    var row1 = 0
+    var row2 = 1
+    while (row1 < 8){
+      board(0)(row1) = '2'
+      board(1)(row2) = '2'
+      board(2)(row1) = '2'
+      row1 += 2
+      row2 += 2
+    }
+    var (row8, row7) = (0, 1)
+    while(row8 < 8){
+      board(7)(row8) = '1'
+      board(6)(row7) = '1'
+      board(5)(row8) = '1'
+      row8 += 2
+      row7 += 2
+    }
+
+
+    return state
+  }
 
 }
