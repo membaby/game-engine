37292545df64d9be7508e5892c0bbc5a208a567b
abderrahman504 <rahmankhsherb@gmail.com>
2023-05-14 20:42:08 +0300

    Added checkers debugger

---
+++ src/main/scala/GameDebuggers/CheckersDebugger.scala
@@ -0,0 +1,27 @@
+package GameDebuggers
+
+object CheckersDebugger {
+  def main(args: Array[String]): Unit = {
+    val controller = Games.Checkers.CheckersController
+    DebugEngine.engine(controller, debugDrawer)
+  }
+
+  private val debugDrawer = (state: Array[Any]) => {
+    val (rows, cols) = (state(0).asInstanceOf[Int], state(1).asInstanceOf[Int])
+    val board = state(3).asInstanceOf[Array[Array[Char]]]
+    printf("%3c", ' ')
+    for (i <- 'a' to 'h'){
+      printf( "%5c", i)
+    }
+    println()
+    for (i <- 0 until rows) {
+      printf("%3d", i)
+      for (j <- 0 until cols) {
+        printf("%5c", board(i)(j))
+      }
+      println()
+    }
+    val playing = state(2).asInstanceOf[Int] % 2 + 1
+    println("Player " + playing + " is playing\n")
+  }
+}
