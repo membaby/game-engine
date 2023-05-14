6160d2a18a0f4192599befed1d07bf1a8feb7e46
abderrahman504 <rahmankhsherb@gmail.com>
2023-05-14 16:41:50 +0300

    Added queens debugger and fixed connect4 debugger

--- src/main/scala/GameDebuggers/QueensDebugger.scala
+++ src/main/scala/GameDebuggers/QueensDebugger.scala
@@ -1,18 +1,19 @@
-package GameDebuggers
+package  GameDebuggers
 
-object Connect4Debugger {
+
+object QueensDebugger {
   def main(args: Array[String]): Unit = {
 
-    val controller = Games.Connect4.ConnectController
+    val controller = Games.Queens.QueensController
     DebugEngine.engine(controller, debugDrawer)
   }
 
-  private val debugDrawer = (state: Array[Any]) =>{
+  private val debugDrawer = (state: Array[Any]) => {
     val (rows, cols) = (state(0).asInstanceOf[Int], state(1).asInstanceOf[Int])
-    val board = state(3).asInstanceOf[Array[Array[Char]]]
+    val board = state(3).asInstanceOf[Array[Array[Boolean]]]
     for (i <- 0 until rows) {
       for (j <- 0 until cols) {
-        printf("%5d", board(i)(j))
+        printf("%10b", board(i)(j))
       }
       println()
     }
